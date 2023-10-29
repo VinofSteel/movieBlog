f045f05dcf75d795965e7805b2ab7e34c6cb5341
VinofSteel <v.nascimento23@yahoo.com.br>
2023-10-29 01:44:21 -0300

    adding logic to display the HTML using go templating, using a bit of mock data to validate concepts

--- cmd/handlers/handlers.go
+++ cmd/handlers/handlers.go
@@ -1,11 +1,25 @@
 package handlers
 
 import (
-	"io"
+	"html/template"
 	"net/http"
 )
 
+type Film struct {
+	Title string
+	Director string
+}
+
 func HandlerHomepage(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello World!\n")
-	io.WriteString(w, r.Method)
+	parsedHtml, err := template.ParseFiles("html/index.html")
+	
+	tmpl := template.Must(parsedHtml, err)
+	films := map[string][]Film{
+		"Films": {
+			{Title: "The Godfather", Director: "Francis Ford Coppola"},
+			{Title: "Blade Runner", Director: "Ridley Scott"},
+			{Title: "The Thing", Director: "John Carpenter"},
+		},
+	}
+	tmpl.Execute(w, films)
 }
